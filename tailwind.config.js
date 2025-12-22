0ba798c3caf2139d18c173ac73164457083a4f65
PiGuy141 <prasadbahekar108@gmail.com>
2025-12-22 20:55:58 +0530

    Add Header UI

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "Arial", "sans-serif"],
+      },
+    },
+  },
+}
