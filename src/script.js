74f8bf792d25849f9e24f87176cafafa572a3a17
PiGuy141 <prasadbahekar108@gmail.com>
2026-01-03 22:44:57 +0530

    Add Video and Playlist with Backend

--- src/script.js
+++ src/script.js
@@ -119,3 +119,73 @@ tags.forEach((tag) => {
 })
 
 document.querySelectorAll(".tags .tag")[0].dataset.active = "true";
+
+
+
+// ! Feed Generation ! //
+
+const video = {
+  type : "video",
+  title : "Building the most INSANE Gaming PC 2026! [ft. ASUS Matrix]",
+  channel : "GeekaWhat",
+  verified : true,
+  views : "13k",
+  time : "18 hours",
+  duration: "18:25",
+  logo : "https://yt3.ggpht.com/Ek9RVVbKa2QCL7ayzGf9_rhCb73dvtpopaZymMZAdCq3qtuHuzucAgFwziThlUUhowgKHLyX7Q=s68-c-k-c0x00ffffff-no-rj",
+  thumbnail: "https://i.ytimg.com/vi/KiBgWRG_SEk/hq720.jpg"
+}
+
+const playlist = {
+  type : "playlist",
+  title : "Building the most INSANE Gaming PC 2026! [ft. ASUS Matrix]",
+  channel : "GeekaWhat",
+  verified : true,
+  thumbnail: "https://i.ytimg.com/vi/KiBgWRG_SEk/hq720.jpg"
+}
+
+function returnBodyStringFromObject(obj) {
+  let result = "";
+  if (obj.type == "video") {
+    result = `
+      <div class="video cursor-pointer py-3 rounded-2xl">
+        <div class="thumbnail relative mb-2">
+            <img class="mb-2 rounded-xl block w-full" src="${obj.thumbnail}" alt="">
+            <p class="duration absolute bottom-2 right-2 text-sm font-bold text-white bg-black opacity-60 py-0.5 px-1 rounded-sm">${obj.duration}</p>
+        </div>
+        <div class="vid-info flex gap-3">
+            <img class="w-9 h-9 rounded-full" src="${obj.logo}" alt="">
+            <div class="text flex-1">
+                <h4 class="font-medium">${obj.title}</h4>
+                <p class="text-gray-500 hover:text-gray-700 inline-block text-sm">${obj.channel} ${obj.verified ? '<i class="ri-checkbox-circle-fill"></i>' : ""}</p>
+                <p class="text-gray-500 text-sm">${obj.views} views &bull; ${obj.time} ago</p>
+            </div>
+            <a href="#"><i class="ri-more-2-line text-xl font-bold"></i></a>
+        </div>
+      </div>
+    `
+  } else if (obj.type == "playlist") {
+    result = `
+      <div class="playlist cursor-pointer py-3  rounded-2xl">
+        <div class="thumbnail relative mb-2">
+            <img class="mb-2 rounded-xl block w-full border-t-3 border-white" src="${obj.thumbnail}" alt="">
+            <p class="duration absolute bottom-2 right-2 text-sm font-bold text-white bg-black opacity-60 py-0.5 px-1 rounded-sm"><i class="ri-bar-chart-fill"></i> Mix</p>
+            <div class="bg-orange-300 w-11/12 absolute -z-1 left-1/2 -translate-x-1/2 -top-1 rounded-lg h-10"></div>
+        </div>
+        <div class="vid-info flex gap-3">
+            <div class="text flex-1">
+                <h4 class="font-medium">${obj.title}</h4>
+                <p class="text-gray-500 text-sm">${obj.channel}</p>
+            </div>
+            <a href="#"><i class="ri-more-2-line text-xl font-bold"></i></a>
+        </div>
+      </div>
+    `
+  }
+
+
+  return result;
+}
+
+const feed_container = document.querySelector(".feed");
+feed_container.innerHTML += returnBodyStringFromObject(playlist);
